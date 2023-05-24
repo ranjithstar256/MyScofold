1b8174ccf9fb792ee9fd44ada2cf4000eebbeb50
Ranjith <ranjithstar256@gmail.com>
2023-05-24 18:53:16 +0530

    i have added comment

--- app/src/main/java/com/example/myscofold/MainActivity.kt
+++ app/src/main/java/com/example/myscofold/MainActivity.kt
@@ -62,15 +62,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             var tex by rememberSaveable { mutableStateOf("") }
-
+// i am from Android studio
             MyScofoldTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-
 
                     Column() {
                         Button(onClick = { tex="morning" }) {
